3c64610ac7f6e6cd399c3dc0b7678c76860a3a51
Awnishyadav9 <awnishyadav4542@gmail.com>
2024-12-10 17:12:07 +0530

    Student Data

--- student_Data_Management_System/staticfiles_build/static/script.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.getElementById('menuToggle');
-const mobileMenu = document.getElementById('mobileMenu');
-const menuItems = document.getElementById('menuItems');
-
-menuToggle.addEventListener('click', () => {
-  mobileMenu.classList.toggle('hidden');
-});
