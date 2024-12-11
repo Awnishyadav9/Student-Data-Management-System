f4b431267ce6bda4bb15b72dc60d197dbe1b2d33
Awnishyadav9 <awnishyadav4542@gmail.com>
2024-12-11 10:50:23 +0530

    Student data Management System

--- student_Data_Management_System/student_Data_Management_System/settings.py
+++ student_Data_Management_System/student_Data_Management_System/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-t3r61as1odbj7*&tr%sq)ntazl37wur@adhdhf(sc7y!w$l@$9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['.vercel.app']
 # ALLOWED_HOSTS = []
 
 
